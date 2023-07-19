a4558652065549ff03a4b484a121e9e09dc6f253
Demoo969 <64582733+Demoo969@users.noreply.github.com>
2023-07-19 22:07:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,32 @@
 BOT_TOKEN="token"
 BOT_OWNER=658606475 #here user id
+MAX_STICKERS = 3
+MAX_ANIMATIONS = 3
+
+# Ключевые слова
+class keywords:
+    hello = ["привет","hello","здравствуй"]
+    fuck = ["когда пик","кагда будит пик"]
+    stream= ["когда стрим"]
+    
+# словарь айди чатов
+class ids :
+    mirai = -1001851731984
+    fox = -1001694646363
+    test= -1001639603287
 
 
 #here stikers id
 class stickers: 
     box=r"CAACAgIAAxkBAAERZalkJHnMDlcY7Z2-McKtn8qKg8fCVAACJxcAAnh3OEptvpYiWDUkCi8E"
+    box_id=""
     smile=r"CAACAgIAAxkBAAERZcFkJIBn4PnfUMV8NqrJOZmkM2yXKQACcQsAAiA36UokmNlUL8LxYS8E"
+    smile_id=""
     stk1=r"CAACAgIAAxkBAAERbE5kJYVYTTs4Lh5LgGB7IKRJT3RSNwACzAwAAsi_6UqnKqm25Su-SC8E"
+    stk1_id=""
     stk2=r""
+    stk2_id=""
     stk3=r""
+    stk3_id=""
     ranni=r"CAACAgIAAxkBAAERbFBkJYWIJiblFlR3I5dHfnPj4xt0KQACIiIAAjL6CEtUXKDxg31FyS8E"
+    ranni_id="AgADIiIAAjL6CEs"
